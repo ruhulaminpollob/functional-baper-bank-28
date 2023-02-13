ac040a7cc391fa2d34b7bfd12d2f3e1ead982ed3
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-13 16:30:56 +0600

    a simple bug fixed

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -8,12 +8,24 @@ function getInputValues(inputFiledId) {
 
 function getElementValues(elementId) {
     const previousElement = document.getElementById(elementId);
-    const elementValueString = previousElement.value;
+    const elementValueString = previousElement.innerText;
     const elementValue = parseFloat(elementValueString)
     return elementValue;
+}
+console.log(getElementValues('deposit-total'));
+
+
+function setElementById(elementId, newElementValue) {
+    const getElementById = document.getElementById(elementId);
+    getElementById.innerText =newElementValue;
 }
 document.getElementById('deposit').addEventListener('click', function () {
     const depositValue =getInputValues('deposit-value');
     const depositElement =getElementValues('deposit-total');
+    console.log(depositElement);
+
+    // const newDepositElement = depositValue + depositElement;
+    
+    // const setDepositElement  =setElementById('deposit-total', newDepositElement);
     
 })
