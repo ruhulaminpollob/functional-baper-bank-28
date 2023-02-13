7edef0385ef18bf9c798a7ae9331ed9c6c1c4099
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-13 17:08:43 +0600

    added withdraw button function dynamic

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -2,7 +2,8 @@ function getInputValues(inputFiledId) {
     const previousValue =document.getElementById(inputFiledId);
     const inputValueString = previousValue.value;
     const inputValue = parseFloat(inputValueString);
-    inputValueString.value ='';
+    previousValue.value ='';
+    
     return inputValue;
 }
 
@@ -10,22 +11,39 @@ function getElementValues(elementId) {
     const previousElement = document.getElementById(elementId);
     const elementValueString = previousElement.innerText;
     const elementValue = parseFloat(elementValueString)
+    
     return elementValue;
 }
-console.log(getElementValues('deposit-total'));
 
-
-function setElementById(elementId, newElementValue) {
+function setElementByIds(elementId, newElementValue) {
+    if (isNaN(newElementValue)) {
+        alert('empty')
+        return;
+    }
     const getElementById = document.getElementById(elementId);
     getElementById.innerText =newElementValue;
 }
+// deposit button
 document.getElementById('deposit').addEventListener('click', function () {
     const depositValue =getInputValues('deposit-value');
     const depositElement =getElementValues('deposit-total');
-    console.log(depositElement);
+    
+    const newDepositElement = depositValue + depositElement;
+    const setDepositElement  =setElementByIds('deposit-total', newDepositElement);
 
-    // const newDepositElement = depositValue + depositElement;
-    
-    // const setDepositElement  =setElementById('deposit-total', newDepositElement);
-    
+    const balanceElement =getElementValues('balance-total');
+    const newBalanceElement = depositValue + balanceElement;
+    const setNewBalance = setElementByIds('balance-total', newBalanceElement);    
+})
+
+// withdraw button
+document.getElementById('withdraw').addEventListener('click', function () {
+    const withdrawValue=getInputValues('withdraw-value');
+    const withdrawElement=getElementValues('withdraw-total');
+    const newWithdrawElement =withdrawValue +withdrawElement;
+    const setWithdrawElement =setElementByIds('withdraw-total', newWithdrawElement)
+
+    const balanceWithdrawElement =getElementValues('balance-total');
+    const newBalanceWithdrawElement =  balanceWithdrawElement - withdrawValue;
+    const setNewBalance = setElementByIds('balance-total', newBalanceWithdrawElement);
 })
